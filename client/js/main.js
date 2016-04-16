c5fbeedc6a4e371c2d9db487613511ac9a1d7298
Chris Grimmett <xtoast@gmail.com>
2016-04-16 20:12:39 +0000

    add sound debugger

--- client/js/main.js
+++ client/js/main.js
@@ -4,37 +4,68 @@
 
 
 console.log('helo world');
-var socket = io.connect(document.location.hostname);
+// var socket = io.connect(document.location.hostname);
 
 
 
-console.log(socket);
-socket.on("all", function(e) {
-    console.log(e);
-});
+// console.log(socket);
+// socket.on("all", function(e) {
+//     console.log(e);
+// });
 
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-});
+// socket.on('news', function (data) {
+//     console.log(data);
+//     socket.emit('my other event', { my: 'data' });
+// });
 
-socket.on("data", function(d) {
-    //console.log(d);
-});
-
+// socket.on("data", function(d) {
+//     //console.log(d);
+//     //sound.play('click');
+// });
 
 
 /** music
  */
  
 var sound = new Howl({
-  urls: ['sounds.mp3', 'sounds.ogg'],
-  sprite: {
-    blast: [0, 2000],
-    laser: [3000, 700],
-    winner: [5000, 9000]
-  }
+    buffer: true,
+    urls: ['/ass/sounds.mp3', '/ass/sounds.ogg'],
+    sprite: {
+        slave1: [100, 1500],
+        peasant1: [1750, 1000],
+        peasant2: [3000, 1000],
+        slave2: [4400, 1000],
+        plebian1: [6000, 1000],
+        plebian2: [9000, 1000],
+        plebian3: [9400, 1000],
+        worshipme: [11400, 1000],
+        die: [13000, 1000],
+        youarenotworthy: [14500, 1500],
+        notworthy: [17200, 1000],
+        kneelbeforeme: [19500, 2000],
+        kneel: [22000, 1000],
+        kree1: [24000, 1000],
+        jaffa: [26000, 1000],
+        kree2: [28500, 1000],
+        youmaygazeuponme: [30000, 2000],
+        iwillletyoulive1: [33000, 2000],
+        iwillletyoulive2: [35500, 2000],
+        dieforme: [38600, 1200],
+        laydownyourlifeforme: [40500, 2000],
+        youwillgiveyourlife: [43000, 2000],
+        fight: [46000, 1000],
+        fightforme: [48400, 1500],
+        fightinmyname: [50000, 2000],
+        click: [52800, 1000],
+        clicks: [53500, 1000],
+        clickecho: [55500, 1000],
+        thud: [56500, 2000]
+    }
 });
+
+
+
+
 
 /**
  * CHART
